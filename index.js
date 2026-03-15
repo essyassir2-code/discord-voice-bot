e3b68c28f54546e695d7b7b4974c8f1851b87b22
essyassir2-code <essyassir2@gmail.com>
2026-03-15 17:24:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+const { joinVoiceChannel } = require("@discordjs/voice");
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates]
 });
 
 client.once("ready", () => {
   console.log("Bot online!");
+
+  const channel = client.channels.cache.get("VOICE_CHANNEL_1471644802247557172");
+
+  if (!channel) return console.log("Voice channel not found");
+
+  joinVoiceChannel({
+    channelId: channel.id,
+    guildId: channel.guild.id,
+    adapterCreator: channel.guild.voiceAdapterCreator
+  });
 });
 
 client.login(process.env.TOKEN);
